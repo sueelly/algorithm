3ab8658a4f63e9f76fb6ad39265897f03037ca40
HJ Kim <63983866+sueelly@users.noreply.github.com>
2026-02-28 23:21:32 +0900

    Time: 50 ms (63.31%) | Memory: 49.3 MB (23.72%) - LeetSync

--- 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
+++ 1423-maximum-number-of-occurrences-of-a-substring/maximum-number-of-occurrences-of-a-substring.java
@@ -2,22 +2,22 @@ class Solution {
     public int maxFreq(String s, int maxLetters, int minSize, int maxSize) {
         // s.length() - minSize + 1개의 substring 생성
         int len = s.length();
-        String[] substrs = new String[len - minSize + 1];
+        String substr;
         Set<Character> alpha = new HashSet<>();
         Map<String, Integer> count = new HashMap<>();
 
         for (int i = 0; i < len - minSize + 1; i++) {
-            substrs[i] = s.substring(i, i + minSize);
+            substr = s.substring(i, i + minSize);
             for (int j = 0; j < minSize; j++) {
-                alpha.add(substrs[i].charAt(j));
+                alpha.add(substr.charAt(j));
                 if (alpha.size() > maxLetters) {
-                    substrs[i] = null;
+                    substr = null;
                     break ;
                 }
             }
-            if (substrs[i] != null) {
-                if (!count.containsKey(substrs[i])) count.put(substrs[i], 1);
-                else count.put(substrs[i], count.get(substrs[i]) + 1);
+            if (substr != null) {
+                if (!count.containsKey(substr)) count.put(substr, 1);
+                else count.put(substr, count.get(substr) + 1);
             }
             alpha.clear();
         }
