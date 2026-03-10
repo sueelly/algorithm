c632fdccc6c58fd6291330f2dc1ce75b0c7d402d
HJ Kim <63983866+sueelly@users.noreply.github.com>
2026-03-10 23:48:47 +0900

    Time: 0 ms (100.00%) | Memory: 42.4 MB (25.18%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,20 +1,12 @@
-// Overflow when using multiplication -> Pascal's Triangle
-// n multiplications vs 2 * n * n additions with O(n^2) space
 class Solution {
     public int uniquePaths(int m, int n) {
-        if (m == 1 || n == 1) return 1;
-        int r = Math.min(m - 1, n - 1);
-        n = m + n - 2;
-        int[][] pascal = new int[n][r + 1];
+        long count = 1;
+        int min = m > n ? n - 1 : m - 1;
+        int all = m + n - 2;
 
-        pascal[0][0] = 1;
-        pascal[0][1] = 1;
-        for (int i = 1; i < n; i++) {
-            pascal[i][0] = 1;
-            for (int j = 1; j <= r; j++) {
-                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];
-            }
+        for (int i = 1; i <= min; i++) {
+            count = count * all-- / i;
         }
-        return pascal[n - 1][r];
+        return (int) count; 
     }
 }
