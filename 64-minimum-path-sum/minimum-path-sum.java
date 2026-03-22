095873d1ac4a5f0e2bb74bcff80eec71c26f04a5
HJ Kim <63983866+sueelly@users.noreply.github.com>
2026-03-23 07:03:07 +0900

    Time: 4 ms (63.90%) | Memory: 50.5 MB (32.86%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < m; i++) {
             if (i > 0) grid[i][0] += grid[i - 1][0];
             for (int j = 1; j < n; j++) {
-                if (i > 0) grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+                if (i > 0) grid[i][j] += grid[i - 1][j] > grid[i][j - 1] ? grid[i][j - 1] : grid[i - 1][j];
                 else grid[i][j] += grid[i][j - 1];
             }
         }
