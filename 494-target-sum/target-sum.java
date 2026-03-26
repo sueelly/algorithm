b2ba47f687055a09c54cb60592d402256845158e
HJ Kim <63983866+sueelly@users.noreply.github.com>
2026-03-26 10:35:06 +0900

    Time: 307 ms (32.47%) | Memory: 43 MB (82.49%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -6,7 +6,11 @@ class Solution {
     public int findTargetSumWays(int[] nums, int target) {
         this.nums = nums;
         n = nums.length;
-        this.target = target;
+        int sum = 0;
+        
+        for (int i = 0; i < n; i++) sum += nums[i];
+        if ((sum - target) % 2 != 0) return 0;
+        this.target = (sum - target) / 2;
 
         return solve(0, 0);
     }
@@ -16,7 +20,7 @@ class Solution {
             if (sum == target) return 1;
             else return 0;
         }
-        
-        return solve(i + 1, sum + nums[i]) + solve(i + 1, sum - nums[i]);
+        if (sum > target) return 0;
+        return solve(i + 1, sum + nums[i]) + solve(i + 1, sum);
     }
 }
